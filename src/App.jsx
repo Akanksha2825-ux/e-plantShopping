e3e93ed3e72f880fde0b838880628a6a46018de1
Akanksha Jadhav <workakanksha133@gmail.com>
2024-10-03 09:57:19 -0400

    Set up Redux store with CartSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,14 @@
+// index.js or App.js (depending on your project structure)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store'; // Import the store we just configured
 
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
-
-function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
-  return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
-
-
+// Wrapping the app with Provider and passing the store
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice'; // Adjust the import path as necessary
+import cartReducer from './CartSlice'; // Adjust the import path to where your CartSlice.js is located
 
+// Configure the store
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
-  },
+    cart: cartReducer, // 'cart' is the key that will be used to reference the cart state in your app
+  }
 });
 
 export default store;
